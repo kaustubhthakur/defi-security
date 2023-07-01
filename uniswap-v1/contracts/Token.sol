9beb48c02965d25a456270d1fcf9cf213a063769
Kaustubh <kaustubhthakur76@gmail.com>
2023-07-01 16:44:35 +0530

    uniswap

---
+++ uniswap-v1/contracts/Token.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+ 
+    constructor() ERC20("UniToken", "UT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
